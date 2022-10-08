046f66ee3f53807007455d33c75f216e929f15eb
Lokeve <Lokeve@vector.com>
2022-10-08 13:51:50 +0800

    frist commit

---
+++ src/main/java/com/cevtor/test.java
@@ -0,0 +1,11 @@
+package com.cevtor;
+
+/**
+ * @author leijinlei
+ * @create 2022-10-08 13:47
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
