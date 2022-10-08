3e72ecf3750e7765cfd74a5a1e9c4fd1c181fb3c
Lokeve <Lokeve@vector.com>
2022-10-08 13:54:20 +0800

    second commit

--- src/main/java/com/cevtor/test.java
+++ src/main/java/com/cevtor/test.java
@@ -7,5 +7,7 @@ package com.cevtor;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello,git!");
+        System.out.println("hello,git!2");
+        System.out.println("hello,git!3");
     }
 }
